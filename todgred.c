a96eb4b127f49da03ff8fe44f4c8d9ea0c1b5d10
quarg2 <245415063+quarg2@users.noreply.github.com>
2026-06-05 23:20:03 +0530

    Fix issues with rectangles and lines where they are drawn incorectly if their second point is smaller than the first

--- todgred.c
+++ todgred.c
@@ -1,14 +1,10 @@
 #include "todgred.h"
 
-#include <math.h>
-#include <stdio.h>
-
 int main(void) {
     Screen screen;
 
+    // Testing
     initScreen(screen);
-
-    // drawCircle(screen, (Circle){10, {20, 20}}, modeDraw);
 
     drawRectangle(screen,
                   (Rectangle){
@@ -16,10 +12,11 @@ int main(void) {
                       {20, 20}
     },
                   modeDraw);
+
     drawRectangle(screen,
                   (Rectangle){
-                      {30, 10},
-                      {10, 30}
+                      {10, 30},
+                      {30, 10}
     },
                   modeDraw);
 
@@ -31,14 +28,11 @@ int main(void) {
 void drawRectangle(Screen screen, Rectangle rect, Mode mode) {
     for (int x = 0; x < COLS; x++) {
         for (int y = 0; y < ROWS; y++) {
-            if ((x == rect.lowerCorner.x && y >= rect.lowerCorner.y
-                 && y <= rect.upperCorner.y)
-                || (y == rect.lowerCorner.y && x >= rect.lowerCorner.x
-                    && x <= rect.upperCorner.x)
-                || (x == rect.upperCorner.x && y <= rect.upperCorner.y
-                    && y >= rect.lowerCorner.y)
-                || (y == rect.upperCorner.y && x <= rect.upperCorner.x
-                    && x >= rect.lowerCorner.x)) {
+            if (EQUAL2(x, rect.lowerCorner.x, rect.upperCorner.x)
+                    && isBetweenEq(y, rect.lowerCorner.y, rect.upperCorner.y)
+                || EQUAL2(y, rect.lowerCorner.y, rect.upperCorner.y)
+                       && isBetweenEq(x, rect.lowerCorner.x,
+                                      rect.upperCorner.x)) {
                 screen[y][x] = DRAW;
             }
         }
@@ -48,9 +42,12 @@ void drawRectangle(Screen screen, Rectangle rect, Mode mode) {
 void drawLine(Screen screen, Line line, Mode mode) {
     for (int y = 0; y < COLS; y++) {
         for (int x = 0; x < ROWS; x++) {
-            if ((y - line.start.y) * (line.end.x - line.start.x)
-                == (line.end.y - line.start.y) * (x - line.start.x)) {
-                screen[y][x] = DRAW;
+            if (isBetweenEq(x, line.start.x, line.end.x)
+                && isBetweenEq(y, line.start.y, line.end.y)) {
+                if ((y - line.start.y) * (line.end.x - line.start.x)
+                    == (line.end.y - line.start.y) * (x - line.start.x)) {
+                    screen[y][x] = DRAW;
+                }
             }
         }
     }
@@ -69,13 +66,19 @@ void drawCircle(Screen screen, Circle circle, Mode mode) {
 
 void printScreen(Screen screen) {
 #ifdef DEBUG
+    printf(" ");
+    for (int i = 0; i < COLS; i++) {
+        printf("%d", (i / 10) % 10);
+    }
+    printf("\n ");
     for (int i = 0; i < COLS; i++) {
         printf("%d", i % 10);
     }
+    printf("\n");
 #endif
     for (int y = 0; y < COLS; y++) {
 #ifdef DEBUG
-        printf("%d", i % 10);
+        printf("%d", y % 10);
 #endif
         for (int x = 0; x < ROWS; x++) {
             putchar(screen[y][x]);
@@ -86,11 +89,22 @@ void printScreen(Screen screen) {
 
 void clearScreen(Screen screen) { initScreen(screen); }
 
-//
 void initScreen(Screen screen) {
     for (int y = 0; y < COLS; y++) {
         for (int x = 0; x < ROWS; x++) {
             screen[y][x] = BLANK;
         }
     }
+}
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int min(int a, int b) { return (a < b) ? a : b; }
+
+bool isBetweenEq(int val, int lower, int upper) {
+    return val >= min(lower, upper) && val <= max(lower, upper);
+}
+
+bool isBetween(int val, int lower, int upper) {
+    return val > min(lower, upper) && val < max(lower, upper);
 }
--- todgred.h
+++ todgred.h
@@ -1,13 +1,20 @@
 #ifndef todgred_header
 #define todgred_header
 
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
 #define COLS 128
 #define ROWS 128
 
 #define BLANK ' '
 #define FILLED '*'
 
+#define EQUAL2(lhs, rhs1, rhs2) ((lhs) == (rhs1) || (lhs) == (rhs2))
+
 #define DRAW (mode == modeDraw) ? FILLED : BLANK
+
 
 // 2D char array storing the canvas
 typedef char Screen[COLS][ROWS];
@@ -43,5 +50,10 @@ void printScreen(Screen screen);
 void drawCircle(Screen screen, Circle circle, Mode mode);
 void drawRectangle(Screen screen, Rectangle rect, Mode mode);
 void drawLine(Screen screen, Line line, Mode mode);
+
+int max(int a, int b);
+int min(int a, int b);
+bool isBetweenEq(int val, int lower, int upper);
+bool isBetween(int val, int lower, int upper);
 
 #endif
